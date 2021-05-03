7d9214e08b01b34cb33d669d448a8c6c4ed6b5f4
Eka-G <ekaglazkovaa@yandex.ru>
2021-05-04 00:25:43 +0300

    feat: add donate modals and correct feedback

--- online-zoo/pages/landing/landing-script.js
+++ online-zoo/pages/landing/landing-script.js
@@ -1,3 +1,14 @@
 const feedbackBtn = document.getElementById("feedback-btn");
+const feedbackLink = document.getElementById("nav-feedback");
+const donateBtn = document.getElementById("donate-btn");
+const donateTabletBtn = document.getElementById("donate-tablet-btn");
+const nextDonateBtn = document.getElementById("next-donate");
 
 feedbackBtn.addEventListener("click", () => openModal("feedback"));
+feedbackLink.addEventListener("click", () => openModal("feedback"));
+donateBtn.addEventListener("click", () => openModal("donate"));
+donateTabletBtn.addEventListener("click", () => openModal("donate"));
+nextDonateBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  openModal("pay")
+});
--- online-zoo/pages/modals.js
+++ online-zoo/pages/modals.js
@@ -2,50 +2,36 @@
 
 const body = document.querySelector(".body");
 const modals = document.querySelector(".js-modals");
-const nameField = document.getElementById("name");
-const emailField = document.getElementById("email");
-const textField = document.getElementById("text-feedback");
-const sendBtn = document.getElementById("send");
-const feedbackLink = document.getElementById("nav-feedback");
-let closeHandler;
+const payModal = document.getElementById("pay-modal");
+const card = document.getElementById("card");
+console.log(111)
+let closeHandler = () => { };
 
 function openModal(name) {
+  closeModal();
   const modal = modals.querySelector(`[data-modal-name="${name}"]`);
-  console.log(modal);
 
   if (!modal) return;
 
-  const modalsClose = document.querySelector(".js-close");
-  const closeModal = () => {
+  const modalsClose = modal.querySelector(".js-close");
+  const closeFn = () => {
     modals.classList.remove("modals--active");
     modal.classList.remove("modals__container--active");
     body.classList.remove("no-scrollable");
-    modalsClose.removeEventListener("click", closeModal);
+    modalsClose.removeEventListener("click", closeFn);
   };
-  closeHandler = closeModal;
+  closeHandler = closeFn;
 
-  modalsClose.addEventListener("click", closeModal);
+  modalsClose.addEventListener("click", closeFn);
 
   modals.classList.add("modals--active");
   modal.classList.add("modals__container--active");
   body.classList.add("no-scrollable");
 }
 
-function closeModal(event) {
+function closeModal() {
   closeHandler();
-  closeHandler = null;
-}
-
-function validateFields() {
-  if (
-    nameField.validity.valid &&
-    emailField.validity.valid &&
-    textField.validity.valid
-  ) {
-    sendBtn.classList.remove("btn--invalid")
-  } else {
-    sendBtn.classList.add("btn--invalid")
-  }
+  closeHandler = () => { };
 }
 
 modals.addEventListener("click", (event) => {
@@ -54,8 +40,28 @@ modals.addEventListener("click", (event) => {
   }
 });
 
-nameField.addEventListener("input", validateFields);
-emailField.addEventListener("input", validateFields);
-textField.addEventListener("input", validateFields);
+function sliceValue(data, num) {
+  return data.slice(0, num);
+}
 
-feedbackLink.addEventListener("click", () => openModal("feedback"));
+function completeValue(data, num) {
+  return "0".repeat(num - data.length) + data;
+}
+
+payModal.addEventListener("input", (event) => {
+  if (event.target.type > "number") {
+    const leng = event.target.dataset.limit;
+
+    if (event.target.value.length !== leng) {
+      event.target.value = sliceValue(event.target.value, leng);
+    }
+  }
+});
+
+payModal.addEventListener("change", (event) => {
+  const leng = event.target.dataset.limit;
+
+  if (event.target.value.length < leng) {
+    event.target.value = completeValue(event.target.value, leng);
+  }
+});
