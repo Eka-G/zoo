e9d2bd92fb941ae74cc80285caa1c9460c3a7ba3
Eka-G <ekaglazkovaa@yandex.ru>
2021-05-05 16:42:12 +0300

    feat: add accordion(all zoos)

--- online-zoo/pages/zoos/zoos-script.js
+++ online-zoo/pages/zoos/zoos-script.js
@@ -1,24 +1,43 @@
 //accordion
 const informationBox = document.querySelector(".information");
-console.log(informationBox)
 
 function toggleAccordion(event) {
   if (event.target.classList.contains("information__title")) {
-    event.target.nextSibling.nextSibling.classList.toggle("information__text-accordion--active");
-    event.target.nextSibling.nextSibling.nextSibling.nextSibling.classList.toggle("information__arrow-wrapper--open");
-    event.target.parentNode.parentNode.classList.toggle("information__block--active");
+    event.target
+      .nextSibling
+      .nextSibling
+      .classList
+      .toggle("information__text-accordion--active");
+    event.target
+      .nextSibling
+      .nextSibling
+      .nextSibling
+      .nextSibling
+      .classList
+      .toggle("information__arrow-wrapper--open");
+    event.target
+      .parentNode
+      .parentNode
+      .classList
+      .toggle("information__block--active");
   } else if (event.target.classList.contains("information__arrow")) {
-    event.target.parentNode.classList.toggle("information__arrow-wrapper--open");
-    event.target.parentNode.previousSibling.previousSibling.classList.toggle("information__text-accordion--active");
-    event.target.parentNode.parentNode.parentNode.classList.toggle("information__block--active")
+    event.target
+      .parentNode
+      .classList
+      .toggle("information__arrow-wrapper--open");
+    event.target
+      .parentNode
+      .previousSibling
+      .previousSibling
+      .classList
+      .toggle("information__text-accordion--active");
+    event.target
+      .parentNode
+      .parentNode
+      .parentNode
+      .classList
+      .toggle("information__block--active")
   }
 }
 
-
 informationBox.addEventListener("click", (event) => toggleAccordion(event));
-
-
-
-
-// information__arrow--open
-// information__text - accordion--active
