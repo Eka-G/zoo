908256638fe7da2a3f6757bd6d8d975cb651b66f
Eka-G <ekaglazkovaa@yandex.ru>
2021-05-06 03:19:18 +0300

    feat: add map drag and drop

--- online-zoo/pages/map/map-script.js
+++ online-zoo/pages/map/map-script.js
@@ -1,27 +1,69 @@
 const mapImage = document.getElementById("map");
 const mapWrapper = document.getElementById("map-wrapper");
+const dndViewport = document.getElementById("dnd-viewport");
+const translateWpapper = document.getElementById("translate");
 
 //MAP SCALE
+
+document.body.style.setProperty("--scale", 1);
 const scaleMinus = document.querySelector(".map__minus");
 const scalePlus = document.querySelector(".map__plus");
-const stepScale = 0.15;
+const stepScale = 0.2;
 let scale = 1;
+let step = 0
 
 const decreaseScale = () => {
-  if (scale <= 0.4) return;
+  if (scale <= 1) return;
 
   scale -= stepScale;
-  mapImage.style.transform = `scale(${scale})`;
+
+  document.body.style.setProperty("--scale", scale);
 }
 
 const increaseScale = () => {
   if (scale >= 1.6) return;
 
   scale += stepScale;
-  mapImage.style.transform = `scale(${scale})`;
+
+  document.body.style.setProperty("--scale", scale);
 }
 
 scaleMinus.addEventListener("click", decreaseScale);
 scalePlus.addEventListener("click", increaseScale);
+
+//---------
+
+//DnD MAP
+let translateX = 0;
+let translateY = 0;
+
+translateWpapper.style.setProperty("transform", `translate(${translateX}px, ${translateY}px)`);
+
+const drag = (event) => {
+
+  translateX += event.movementX;
+  translateY += event.movementY;
+
+  translateWpapper.style.setProperty("transform", `translate(${translateX}px, ${translateY}px)`);
+}
+
+const leaveDrag = (event) => {
+  stopDrag();
+}
+
+const startDrag = (event) => {
+  dndViewport.addEventListener("mousemove", drag);
+  dndViewport.addEventListener("mouseleave", leaveDrag);
+}
+
+const stopDrag = (event) => {
+  dndViewport.removeEventListener("mousemove", drag);
+  dndViewport.removeEventListener("mouseleave", leaveDrag);
+}
+
+dndViewport.ondragstart = () => false;
+
+dndViewport.addEventListener("mousedown", startDrag);
+dndViewport.addEventListener("mouseup", stopDrag);
 
 //---------
